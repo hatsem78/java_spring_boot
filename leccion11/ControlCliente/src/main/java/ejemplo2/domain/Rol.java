a0c9aca0913c7ced357adc862a34739389d7394a
Hatsembiller Octavio Daniel <hatsem78@gmail.com>
2021-06-16 10:06:16 -0300

    Lección 10.  Seguridad con Spring Framework y Spring Security

---
+++ leccion11/ControlCliente/src/main/java/ejemplo2/domain/Rol.java
@@ -0,0 +1,28 @@
+package ejemplo2.domain;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import lombok.Data;
+
+/**
+ *
+ * @author octavio
+ */
+
+@Entity
+@Data 
+@Table(name = "rol")
+public class Rol implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idRol;
+    
+    @NotEmpty
+    private String nombre;
+    
+}
