b9fce7a9dbcd22138d5c9a60b261a93500a084ea
Hatsembiller Octavio Daniel <hatsem78@gmail.com>
2021-06-16 10:05:17 -0300

    Lección 10.  Seguridad con Spring Framework y Spring Security

--- leccion10/ControlCliente/src/main/java/ejemplo2/web/ControladorInicio.java
+++ leccion10/ControlCliente/src/main/java/ejemplo2/web/ControladorInicio.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -29,9 +31,10 @@ public class ControladorInicio {
      * @return
      */
     @GetMapping("/")
-    public String inicio(Model model){
+    public String inicio(Model model, @AuthenticationPrincipal User user){
         var personas = personaService.listarPersonas();
         log.info("ejecutando el controlador Spring MVC");
+        log.info("usuario que hizo login" + user);
         model.addAttribute("personas", personas);
         return "index";
     }
